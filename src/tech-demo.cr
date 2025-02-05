4d15cc2e9bb2bdac59229be97c274b9bd0ab258e
jaskinner <jon.a.skinner1@gmail.com>
2025-02-05 15:32:57 +0000

    Created endpoints for data and front end

--- src/tech-demo.cr
+++ src/tech-demo.cr
@@ -1,6 +1,30 @@
-# TODO: Write documentation for `Tech::Demo`
-module Tech::Demo
-  VERSION = "0.1.0"
+require "http/server"
+require "./init_db"
+require "./db"
+require "./scraper"
 
-  # TODO: Put your code here
+server = HTTP::Server.new do |context|
+  if context.request.path == "/prices"
+    context.response.content_type = "application/json"
+    context.response.print fetch_prices
+    next
+  elsif context.request.path == "/scraper"
+    update_db
+    context.response.print "Prices updated"
+    next
+  elsif context.request.path == "/favicon.ico"
+    context.response.print ""
+    next
+  elsif context.request.path == "/"
+    context.response.content_type = "text/html"
+    context.response.print File.read("public/index.html")
+    next
+  else
+    context.response.status_code = 404
+    context.response.print "Not found"
+  end
 end
+
+address = server.bind_tcp("0.0.0.0", 8080)
+puts "Listening on http://#{address}"
+server.listen
